91d7820a8f1f6ac8dbb778731e5e657dd4365c18
a_liYa <a_liYa@126.com>
2020-12-11 09:08:13 +0800

    添加支持 RelativeLayout

--- library/src/main/java/com/aliya/view/fitsys/FitWindowsRelativeLayout.java
+++ library/src/main/java/com/aliya/view/fitsys/FitWindowsRelativeLayout.java
@@ -1,67 +1,66 @@
-//package com.aliya.view.fitsys;
-//
-//import android.content.Context;
-//import android.graphics.Rect;
-//import android.os.Build;
-//import android.util.AttributeSet;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.view.WindowInsets;
-//import android.widget.RelativeLayout;
-//
-///**
-// * 自定义拓展FitSystemWindow属性 - LinearLayout.
-// *
-// * @author a_liYa
-// * @date 2017/7/19 12:40.
-// * @see FitHelper
-// */
-//public class FitWindowsRelativeLayout extends RelativeLayout implements FitHelper.FitWindowsProxy {
-//
-//    private FitHelper helper;
-//
-//    public FitWindowsRelativeLayout(Context context) {
-//        this(context, null);
-//    }
-//
-//    public FitWindowsRelativeLayout(Context context, AttributeSet attrs) {
-//        this(context, attrs, 0);
-//    }
-//
-//    public FitWindowsRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
-//        super(context, attrs, defStyleAttr);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//            helper = new FitHelper(context, attrs, this);
-//        }
-//    }
-//
-//    @Override
-//    public void addView(View child, int index, ViewGroup.LayoutParams params) {
-//        super.addView(child, index, params);
-//        if (helper != null) {
-//            helper.fitInsetsChildView(child);
-//        }
-//    }
-//
-//    @Override
-//    public WindowInsets dispatchApplyWindowInsets(WindowInsets insets) {
-//        if (helper != null) {
-//            helper.setWindowInsets(insets);
-//        }
-//        return super.dispatchApplyWindowInsets(insets);
-//    }
-//
-//    @Override
-//    protected boolean fitSystemWindows(Rect insets) {
-//        if (helper != null) {
-//            return helper.fitSystemWindows(insets);
-//        }
-//        return super.fitSystemWindows(insets);
-//    }
-//
-//    @Override
-//    public boolean fitSystemWindowsProxy(Rect insets, boolean callSuper) {
-//        return callSuper ? super.fitSystemWindows(insets) : fitSystemWindows(insets);
-//    }
-//
-//}
+package com.aliya.view.fitsys;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.RequiresApi;
+
+/**
+ * 自定义拓展FitSystemWindow属性 - LinearLayout.
+ *
+ * @author a_liYa
+ * @date 2017/7/19 12:40.
+ * @see FitHelper
+ */
+public class FitWindowsRelativeLayout extends RelativeLayout {
+
+    private FitHelper helper;
+
+    public FitWindowsRelativeLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public FitWindowsRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            helper = new FitHelper(this, attrs, new FitHelper.FitWindowsProxy() {
+                @Override
+                public boolean fitSystemWindowsProxy(Rect insets, boolean callSuper) {
+                    return callSuper ?
+                            FitWindowsRelativeLayout.super.fitSystemWindows(insets)
+                            : fitSystemWindows(insets);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        super.addView(child, index, params);
+        if (helper != null) {
+            helper.fitInsetsChildView(child);
+        }
+    }
+
+    @Override
+    public android.view.WindowInsets dispatchApplyWindowInsets(android.view.WindowInsets insets) {
+        if (helper != null) {
+            helper.setWindowInsets(insets);
+        }
+        return super.dispatchApplyWindowInsets(insets);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected boolean fitSystemWindows(Rect insets) {
+        if (helper != null) {
+            return helper.fitSystemWindows(insets);
+        }
+        return super.fitSystemWindows(insets);
+    }
+}
